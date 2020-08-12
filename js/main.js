89eba680e1fc7836d0e2f329d460eb4f08fe7916
AntonKurtsev <kucrev@gmail.com>
2020-08-12 03:00:06 +0300

    create: add tabs section

--- js/main.js
+++ js/main.js
@@ -3,4 +3,15 @@ $(document).ready(function () {
     $('.header-menu').toggleClass('header-menu--mobile-visible');
     $('.header-menu__list').toggleClass('header-menu__list--mobile-visible');
   })
+
+  var tabsItem = $('.tabs-menu__item');
+  var tabsContent = $('.recomended__tabs-content');
+
+  tabsItem.on('click', function (event) {
+    var activeContent = $(this).attr('data-target')
+    tabsItem.removeClass('tabs-menu__item--active');
+    $(this).addClass('tabs-menu__item--active');
+    tabsContent.removeClass('recomended__tabs-content--active');
+    $('#' + activeContent).addClass('recomended__tabs-content--active');
+  })
 });
