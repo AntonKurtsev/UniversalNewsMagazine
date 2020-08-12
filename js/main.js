39dd85ae0b0dc36208eb706f6d7621317c353c22
AntonKurtsev <kucrev@gmail.com>
2020-08-12 13:55:21 +0300

    create: add video in recomended

--- js/main.js
+++ js/main.js
@@ -14,4 +14,7 @@ $(document).ready(function () {
     tabsContent.removeClass('recomended__tabs-content--active');
     $('#' + activeContent).addClass('recomended__tabs-content--active');
   })
+
+  $(".fancybox").fancybox();
+
 });
