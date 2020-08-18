b3e0a28f61f9ffda38e738cd11af5bbeb5c173e8
AntonKurtsev <kucrev@gmail.com>
2020-08-18 15:31:37 +0300

    fixx: add hover for all cards & fixed bugs in js

--- js/main.js
+++ js/main.js
@@ -110,7 +110,7 @@ $(document).ready(function () {
   $('.parallax-window').parallax({imageSrc: 'img/parallax-bg.jpg'});
 
   $('.load-more').on('click', function() {
-    $('.article-comments-hide').toggleClass('article-comments-hide-active').slideToggle(500);
+    $('.article-comments-hide').slideToggle(500);
     $('.load-more__text').toggleClass('load-more__text--hide');
     $('.load-more__icon').toggleClass('load-more__icon--hide');
   })
